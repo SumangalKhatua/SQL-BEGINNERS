ebd387256daf3d111098a1623631a9d4c888230e
Sumangal khatua <117670408+SumangalKhatua@users.noreply.github.com>
2023-02-15 17:08:31 +0530

    Add files via upload

---
+++ SQL basics select and from statement.sql
@@ -0,0 +1,45 @@
+--Select *
+--From EmployeeDemographics;
+--(for all the data use *)
+
+--Select firstname,lastname
+--From EmployeeDemographics;
+--(for selected data)
+
+--Select Top 5 *
+--From EmployeeDemographics;
+--(get top of all)
+
+--select Distinct(gender)
+--from employeedemographics;
+--(to get unique value of tables)
+
+--select Count(lastname)
+--from employeedemographics;
+--(it will count how many object have in the column but it will not show the name of the column name)
+
+
+--select Count(lastname) as lastnamecount
+--from employeedemographics;
+--(it will count how many object have in the column but it will show the name of the column name)
+
+--select max(salary)
+--from EmployeeSalary
+--;
+
+--select min(salary)
+--from EmployeeSalary
+--;
+
+--select avg(salary)
+--from EmployeeSalary
+--;
+
+--select *
+--from EmployeeSalary
+--;
+
+
+--select *
+--from SQLBEGINNERS.dbo.EmployeeSalary;
+--(it is use to execute query of different database)
