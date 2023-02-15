c8e37d02ee0b70de07b0c272b0cf25c776e099b3
Sumangal khatua <117670408+SumangalKhatua@users.noreply.github.com>
2023-02-15 17:33:49 +0530

    Add files via upload

---
+++ SQL basics Where,Wildcard, IN  statement.sql
@@ -0,0 +1,76 @@
+--select *
+--from EmployeeDemographics
+--where firstname ='jim';
+--(equal)
+
+--select *
+--from EmployeeDemographics
+--where firstname !='jim';
+
+--select *
+--from EmployeeDemographics
+--where firstname <>'jim';
+--(both same not equal)
+
+--select *
+--from EmployeeDemographics
+--where age > 30;
+--greater than
+
+--select *
+--from EmployeeDemographics
+--where age >= 30;
+--greater than equal
+
+
+--select *
+--from EmployeeDemographics
+--where age < 33;
+--less than
+
+--select *
+--from EmployeeDemographics
+--where age <= 33;
+----less than equal
+
+
+--select *
+--from EmployeeDemographics
+--where age < 33 and gender = 'male';
+
+--select *
+--from EmployeeDemographics
+--where age < 33 or gender = 'male';
+
+--select *
+--from EmployeeDemographics
+--where lastname like 's%';
+
+--select *
+--from EmployeeDemographics
+--where lastname like '%s%';
+
+--select *
+--from EmployeeDemographics
+--where lastname like '%y';
+
+--select *
+--from EmployeeDemographics
+--where lastname like 's%c%e';
+
+--select *
+--from EmployeeDemographics
+--where firstname is null;
+
+--select *
+--from EmployeeDemographics
+--where firstname is not null;
+
+--select *
+--from EmployeeDemographics
+--where firstname = 'jim' or firstname = 'pam' or firstname = 'michael';
+
+--select *
+--from EmployeeDemographics
+--where firstname in ('jim','pam','michael')
+
