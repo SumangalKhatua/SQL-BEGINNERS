ed8c9d58bf8d3af48d17ea4409c8db7934cff6b8
Sumangal khatua <117670408+SumangalKhatua@users.noreply.github.com>
2023-02-16 23:50:28 +0530

    Add files via upload

---
+++ SQL basics Group by,Order by.sql
@@ -0,0 +1,72 @@
+
+
+
+--select gender from sqlbeginners.dbo.employeedemographics;
+
+
+
+
+--select gender, Count(gender) as Count 
+--from EmployeeDemographics
+--group by Gender
+
+--select gender , age,count(gender) as count_of_unique_gender , count(age) as count_of_unique_age
+--from EmployeeDemographics
+--group by gender , age
+
+
+--select gender, Count(gender) as Count 
+--from EmployeeDemographics
+--where age>31
+--group by Gender
+
+--select gender, Count(gender) as Count_gender 
+--from EmployeeDemographics
+--where age>31
+--group by Gender
+--order by Count_gender
+
+--select gender, Count(gender) as Count_gender 
+--from EmployeeDemographics
+--where age>31
+--group by Gender
+--order by Count_gender asc
+
+--select gender, Count(gender) as Count_gender 
+--from EmployeeDemographics
+--where age>31
+--group by Gender
+--order by Count_gender desc
+
+--select gender, Count(gender) as Count_gender 
+--from EmployeeDemographics
+--where age>31
+--group by Gender
+--order by gender asc
+
+--select gender, Count(gender) as Count_gender 
+--from EmployeeDemographics
+--where age>31
+--group by Gender
+--order by gender desc
+
+
+--select * 
+--from EmployeeDemographics
+--order by age
+
+--select * 
+--from EmployeeDemographics
+--order by age desc
+
+--select * 
+--from EmployeeDemographics
+--order by age,gender desc
+
+--select * 
+--from EmployeeDemographics
+--order by age desc,gender desc
+
+--select * 
+--from EmployeeDemographics
+--order by 4 desc,5 desc
